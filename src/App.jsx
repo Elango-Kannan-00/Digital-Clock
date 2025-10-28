713419b3111f63cce5c62a619fd250fe1a67c11c
Elango-Kannan-00 <elango.kiot@gmail.com>
2025-10-28 20:37:02 +0530

    feat: added App.jsx file for clock component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,33 @@
 import React, { useState, useEffect } from "react";
 
+import "./App.css";
+
 const App = () => {
-  const [currentTime, setCurrentTime] = useState(Date.now());
+  const [currentTime, setCurrentTime] = useState(new Date());
 
   useEffect(() => {
     const timer = setInterval(() => {
-      setCurrentTime(Date.now())
-    }) 
-  })
+      setCurrentTime(new Date());
+    }, 1000);
 
-}
+    return () => clearInterval(timer);
+  }, []);
+
+  let hour = currentTime.getHours();
+  let minutes = currentTime.getMinutes();
+  let seconds = currentTime.getSeconds();
+
+  hour = hour < 10 ? "0" + hour : hour;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
 
   return (
-    <div>
-      
+    <div className="body">
+      <h1 className="clock-container">
+        {hour}:{minutes}:{seconds}
+      </h1>
     </div>
-  )
+  );
+};
 
-export default App
+export default App;
